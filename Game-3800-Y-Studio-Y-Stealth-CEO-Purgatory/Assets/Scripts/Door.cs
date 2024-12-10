ece2982721872b8474f9f4a2f6cd16a85f8e9381
Bosspug <eugeniojlopezt@gmail.com>
2024-12-10 14:30:03 -0500

    Added sfx to doors, balanced some audio, added basic loop music
    demon scream was way too loud lmao genuine jumpscare

--- Game-3800-Y-Studio-Y-Stealth-CEO-Purgatory/Assets/Scripts/Door.cs
+++ Game-3800-Y-Studio-Y-Stealth-CEO-Purgatory/Assets/Scripts/Door.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] Transform point;
+
+    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource doorSfx = GetComponent<AudioSource>();
+        doorSfx.Play();
         collision.gameObject.transform.position = point.position;
     }
 }
